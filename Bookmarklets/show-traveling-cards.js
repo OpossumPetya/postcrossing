11c70a404a26b470abc597424d08686e30925a4d
OpossumPetya <russianspy@narod.ru>
2016-07-19 23:29:34 +0300

    Now selected columns are not important, and works on Sent/Received pages

--- Bookmarklets/show-traveling-cards.js
+++ Bookmarklets/show-traveling-cards.js
@@ -1,7 +1,8 @@
 
-// Execute from Traveling Postcards page
-// Following columns are expectrd: ID, To Member, To Country, Sent, Traveled (non default), Img
-// You may also want to select Display All Cards if you have more than 25 traveling cards
+// Execute from Traveling/Sent/Received Postcards page
+// Img column is required, all other are optional
+// You may also want to select Display All Cards if you have more than 25 traveling cards.
+// Expired cards' travel days is highlighted.
 
 javascript:(function(){
 // ========================================================
@@ -56,14 +57,18 @@ document.getElementsByTagName('head')[0].appendChild(new_style);
 
 // ---------------------------------------------
 
+var idTemplate = '<b class=\'cardId\'>%CARDID%</b>';
+var traveledTemplate = '<span class=\'cardTraveled\'>%TRAVELED%</span>';
+var userTemplate = '%USER%';
+var countryTemplate = '%COUNTRY_FLAG%&nbsp;%COUNTRY%';
+
+var separatorTemplate = ' <span style=\'color:#000\'>&#8226;</span> ';
+
 var cardInfoTemplate = ' \
 <div class=\'cardContainer\'> \
   <div class=\'cardInfoWrapper\'> \
       <div class=\'cardInfo\'> \
-        <b class=\'cardId\'>%CARDID%</b> \
-        <span style=\'color:#000\'>&#8226;</span> <span class=\'cardTraveled\'>%TRAVELED%</span> \
-        <span style=\'color:#000\'>&#8226;</span> %USER% \
-        <span style=\'color:#000\'>&#8226;</span> %COUNTRY_FLAG%&nbsp;%COUNTRY% \
+        %INFO% \
       </div> \
   </div> \
   %POSTCARD% \
@@ -72,6 +77,9 @@ var cardInfoTemplate = ' \
 
 // ---------------------------------------------
 
+// get available columns
+var column = {};
+$('.dataTables_scrollHead th').each(function(key, value) {
 
 var id = [],
     to_member = [],
@@ -85,22 +93,38 @@ var id = [],
 window.src = {};    // is global, so we can accesss it from asynch functions
 
 $('#postcardTable > tbody > tr').each(function() {
-    id.push($(this).find('td:nth-child(1) > a').text());
-    to_member.push($(this).find('td:nth-child(2) > a').text());
-    to_country.push($(this).find('td:nth-child(3) > a').text());
-    to_country_code.push($(this).find('td:nth-child(3) > a').attr('href').match(/\w\w$/));
-    to_country_flag_img.push($(this).find('td:nth-child(3) > img').addClass('zoomFlag').prop('outerHTML'));
-    sent_date.push($(this).find('td:nth-child(4)').text());
-    travel_days.push($(this).find('td:nth-child(5)').text());
-    img_link.push($(this).find('td:nth-child(6) > a'));
+    if (column['Postcard ID'])
+        id.push($(this).find('td:nth-child('+column['Postcard ID']+') > a').text());
+    if (column['Member'])
+        to_member.push($(this).find('td:nth-child('+column['Member']+') > a').text());
+    if (column['Country']) {
+        to_country.push($(this).find('td:nth-child('+column['Country']+') > a').text());
+        to_country_code.push($(this).find('td:nth-child('+column['Country']+') > a').attr('href').match(/\w\w$/));
+        to_country_flag_img.push($(this).find('td:nth-child('+column['Country']+') > img').addClass('zoomFlag').prop('outerHTML'));
+    }
+    if (column['Sent'])
+        sent_date.push($(this).find('td:nth-child('+column['Sent']+')').text());
+    if (column['Traveled'])
+        travel_days.push($(this).find('td:nth-child('+column['Traveled']+')').text());
+    if (column['Img']) {
+        if (/traveling/.test(window.location.href))
+            img_link.push($(this).find('td:nth-child('+column['Img']+') > a'));
+        else
+            img_link.push($(this).find('td:nth-child('+column['Postcard ID']+') > a'));
+    }
 });
 
 // this gets called for each $.get(), as they finish downloading content
 $(document).ajaxComplete(function(event, xhr, settings) {
     var n = getURLParameter(settings.url, 'num');
     var response = $('<html />').html(xhr.responseText);
-    var source = response.find('img');
-    window.src[n] = source[0].outerHTML;
+    var source = response.find('img.image-with-shadow');
+    if (source.length == 1)         // traveling card page
+        window.src[n] = source[0].outerHTML;
+    else if (source.length >= 3)    // sent or received card page (may have comments with user image)
+        window.src[n] = source[2].outerHTML;
+    else                            // card page without image uploaded
+        window.src[n] = '';
 });
 
 for (var i = 0; i < img_link.length; i++) {
@@ -110,17 +134,22 @@ for (var i = 0; i < img_link.length; i++) {
 // this gets called when all ajax requests are done
 $(document).ajaxStop(function () {
     $('#postcardTable_wrapper').empty();
-    for (var i = 0; i < id.length; i++)
+    for (var i = 0; i < img_link.length; i++)
+    {
+        var info = [];
+    
+        if (column['Postcard ID'])  info.push(TemplateEngine(idTemplate, { CARDID: id[i] }));
+        if (column['Traveled'])     info.push(TemplateEngine(traveledTemplate, { TRAVELED: travel_days[i] }));
+        if (column['Member'])    info.push(TemplateEngine(userTemplate, { USER: to_member[i] }));
+        if (column['Country'])   info.push(TemplateEngine(countryTemplate, { COUNTRY_FLAG: to_country_flag_img[i], COUNTRY: to_country[i] }));
+        
+        var info_combined = '';
+        if (info.length) info_combined = info.join(separatorTemplate);
+
         $('#postcardTable_wrapper').append(
-            TemplateEngine(cardInfoTemplate, {
-                CARDID: id[i],
-                TRAVELED: travel_days[i],
-                USER: to_member[i],
-                COUNTRY_FLAG: to_country_flag_img[i],
-                COUNTRY: to_country[i],
-                POSTCARD: window.src[i]
-            })
-    );
+            TemplateEngine(cardInfoTemplate, { INFO: info_combined, POSTCARD: window.src[i] })
+        );
+    }
     $('.cardTraveled:contains(\'expired\')').addClass('cardExpired');
 });
 
