3d83c3c765ad26224b369c42ea278dd2a9dd8365
OpossumPetya <russianspy@narod.ru>
2016-11-01 19:17:26 +0300

    Fixed broken CSS selector
    * Fixed broken CSS selector
    * Not it should be possible to run it from any Postcrossing page that
    loads JQuery

--- Bookmarklets/random-postcard-game.js
+++ Bookmarklets/random-postcard-game.js
@@ -3,6 +3,9 @@ javascript:(function(){
 
 // Execute from Traveling Postcards page
 
+if (!window.jQuery)
+    $("#mainContentArea").prepend("<div class='success'><b>Try running this from traveling postcards page!</b></div>");
+
 var MAX_GALLERY_PAGE = 62500;
 
 function randomInt(min, max) {
@@ -10,19 +13,19 @@ function randomInt(min, max) {
 }
 
 window.next_random_postcard = function() {
-    $('#postcardTable_wrapper').empty();
+    $('#mainContentArea').empty();
     // get random gallery page
     $.get('https://www.postcrossing.com/gallery/' + randomInt(1,MAX_GALLERY_PAGE), function(data) {
         var gallery_page_obj = $('<html />').html(data);
         // get URLs of all images
-        var img_source = gallery_page_obj.find("#postcardImageList li a:last-child").map( function() { return $(this).attr('href'); }).get();
+        var img_source = gallery_page_obj.find("ul.postcardImageList li a:last-child").map( function() { return $(this).attr('href'); }).get();
         // get random image page (from the gallery page retrieved above)
         $.get(location.origin + img_source[randomInt(1,img_source.length)], function(data) {
             var postcard_page_obj = $('<html />').html(data).find("#postcardControls");
             postcard_page_obj.find("td:nth-child(2)").empty(); // remove social media links
             // ... and add "refresh" icon
             postcard_page_obj.find("td:nth-child(2)").append("<a href='javascript:next_random_postcard();'><img alt='Show another random postcard' title='Show another random postcard' src='//static1.postcrossing.com/images/indicator.gif' /></a>");
-            $("#postcardTable_wrapper").append( postcard_page_obj );
+            $("#mainContentArea").append( postcard_page_obj );
         });
     });
 }
