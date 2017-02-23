f4e9381d6848a89b407ba4cc8e66f02e5dd359ed
OpossumPetya <russianspy@narod.ru>
2017-02-24 02:12:37 +0300

    update selectors per changes on site

--- Bookmarklets/show-postcard-url.js
+++ Bookmarklets/show-postcard-url.js
@@ -1,8 +1,8 @@
 
 // Execute from individual received/sent Postcard page ( https://www.postcrossing.com/postcards/XX-##### )
-// Does not work on traveling Postcard page
+// Does not work on traveling postcard page, but on that page right-click menu is not disabled
 
 javascript:(function(){
-    var url = $("#postcardControls img[class*='image-with-shadow']").attr("src");
-    $("#postcardControls img[class*='image-with-shadow']").after("<p><br/><input type='text' style='width:500px' onClick='this.select();' value='https:" + url + "' /></p>")
+    var url = location.protocol + $("img[class*='postcard-image']").attr("src");
+    $("img[class*='postcard-image']").after("<p><br/><input type='text' style='width:500px' onClick='this.select();' value='" + url + "' /></p>");
 })();
